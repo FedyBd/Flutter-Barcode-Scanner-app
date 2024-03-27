265adcc0c524c7840b0435cd7595e4812a56f03d
FedyBd <fedibouabid49@gmail.com>
2024-03-27 16:48:35 +0100

    flutter app

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Barcode Scanner',
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  String _scanBarcodeResult = '';
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6656',
+        'Cancel',
+        true,
+        ScanMode.BARCODE,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+    setState(() {
+      _scanBarcodeResult = barcodeScanRes;
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('Barcode Scan'),
+    ),
+    body: Builder(
+      builder: (context) => Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => scanBarcodeNormal(),
+              child: const Text('Start scan barcode'),
+            ),
+            Text('Scan barcode Result: $_scanBarcodeResult\n')
+          ],
+        ),
+      ),
+    ),
+  );
+}
